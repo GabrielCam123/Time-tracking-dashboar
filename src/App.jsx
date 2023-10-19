4ad2881e9bc3c439658cf89dcd3d9b94c03b1d71
OscarCameo <cameotapia@gmail.com>
2023-10-18 23:35:28 -0400

    Prueba de botones 1

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Perfil from './components/Perfil.jsx'
+import Botones from './components/Botones'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,6 +12,7 @@ function App() {
     <>
       <div>
       <Perfil/>
+      <Botones/>
       </div>
     </>
   )
--- src/components/Botones.jsx
+++ src/components/Botones.jsx
@@ -1,6 +1,13 @@
 import React from "react"
 import './styles/Botones.css'
 const Botones=()=>{
+    return(
+        <>
+        <button>Daily</button>
+        <button>Weekly</button>
+        <button>Monthly</button>
+        </>
+    )
 
 }
 export default Botones
